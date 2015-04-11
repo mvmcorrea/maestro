91608c9353f3f934299507e8853531f364863c4c
Daniel Schmitz <danieljfa@gmail.com>
2015-04-11 01:51:55 -0300

    Teste

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ ini_set("error_reporting", "E_ALL & ~E_NOTICE & ~E_STRICT");
 //ini_set("display_errors", "on");
 ini_set("log_errors","on");
 ini_set("error_log","core/var/log/php_error.log");
-//teste
+
 // change the following paths if necessary
 $dir = dirname(__FILE__);
 $conf = dirname(__FILE__).'/core/conf/conf.php';
